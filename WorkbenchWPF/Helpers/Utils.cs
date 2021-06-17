22e34996d0a7f36d42e47648958f267655eeeca0
Kecy Jones <Miskecysss@gmail.com>
2021-06-17 14:35:51 -0300

    Add Helper class util

---
+++ WorkbenchWPF/Helpers/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkbenchWPF.Helpers
+{
+    public class Utils
+    {
+
+        public decimal StringToDecimal(string text)
+        {            
+            try
+            {
+                return Convert.ToDecimal(text, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                decimal result;
+                decimal.TryParse(text, out result);
+                return result;
+            }
+        }
+
+        public int StringToInteger(string text)
+        {
+            try
+            {
+                return Convert.ToInt32(text);
+            }
+            catch
+            {
+                int result;
+                int.TryParse(text, out result);
+                return result;
+            }
+        }
+
+        public decimal CalcWinrate(decimal win, decimal loss)
+        {
+            decimal x = win > 0 ? (win / (loss + win)) * 100 : 0;
+            return x;
+        }
+    }
+}
